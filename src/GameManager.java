657e3455ba37a692a9665f03f3b8d88efa7af380
ogurcan1 <ogurcan1@corp.vodafone.es>
2023-04-03 02:46:49 +0300

    foreground and background color

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,5 +1,6 @@
 package gravity.src;
 
+import enigma.console.TextAttributes;
 import enigma.core.Enigma;
 import enigma.event.TextMouseEvent;
 import enigma.event.TextMouseListener;
@@ -8,15 +9,36 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
 
+import static java.awt.Color.*;
+
 public class GameManager {
     public static final int ROBOT_COUNT = 7;
     public static final int GAME_FIELD_X = 55;
     public static final int GAME_FIELD_Y = 25;
+
+    public static final TextAttributes PLAYER_COLOR = new TextAttributes(GREEN, BLACK);
+    public static final TextAttributes ROBOT_COLOR = new TextAttributes(YELLOW, BLACK);
+    public static final TextAttributes WALL_COLOR = new TextAttributes(WHITE, BLACK);
+    public static final TextAttributes EARTH_COLOR = new TextAttributes(BLACK, WHITE);
+    public static final TextAttributes BOULDER_COLOR = new TextAttributes(BLACK, WHITE);
+    public static final TextAttributes TREASURE_COLOR = new TextAttributes(RED, WHITE);
+    public static final TextAttributes CONSOLE_COLOR = new TextAttributes(BLACK, WHITE);
     Random rnd = new Random();
     public int px, py; // player x,y position
     char[][] wholeGrid = new char[GAME_FIELD_X][GAME_FIELD_Y];
 
-    public enigma.console.Console cn = Enigma.getConsole("Gravity - Team7", 75, GAME_FIELD_Y);
+    private static final enigma.console.Console cn;
+
+    static {
+        cn = Enigma.getConsole("Gravity - Team7", 75, GAME_FIELD_Y);
+        cn.setTextAttributes(CONSOLE_COLOR);
+        for (int i = 0; i < cn.getTextWindow().getColumns(); i++) {
+            for (int j = 0; j < cn.getTextWindow().getRows(); j++) {
+                cn.getTextWindow().output(i, j, ' ', CONSOLE_COLOR);
+            }
+        }
+    }
+
     public TextMouseListener tmlis;
     public KeyListener klis;
 
@@ -28,7 +50,7 @@ public class GameManager {
     // ----------------------------------------------------
 
 
-    private void doNotChange(){
+    private void doNotChange() {
         // ------ Standard code for mouse and keyboard ------ Do not change
         tmlis = new TextMouseListener() {
             public void mouseClicked(TextMouseEvent arg0) {
@@ -64,29 +86,24 @@ public class GameManager {
         cn.getTextWindow().addKeyListener(klis);
         // ----------------------------------------------------
     }
+
     public GameManager() throws Exception { // --- Contructor
+
         doNotChange();
-
-
         //1.	Walls are placed.
         //2.	All empty squares are converted into earth squares.
         //3.	Random 180 earth squares are converted into boulders.
         //4.	Random 30 earth squares are converted into treasures (Random 1, 2 or 3 with equal probability).
         //5.	Random 200 earth squares are converted into empty squares.
-
         initializeWallAndEarth();
-
         //6.	Random 7 earth squares are converted into robots.
         initializeRobots();
-
         //7.	Player P is placed on a random earth square.
         initializePlayer();
 
-
-
         while (true) {
 //            if (mousepr == 1) { // if mouse button pressed
-//                cn.getTextWindow().output(mousex, mousey, '#'); // write a char to x,y position without changing cursor
+//                cn.getTextWindow().output(mousex, mousey, '#',WALL_COLOR); // write a char to x,y position without changing cursor
 //                // position
 //                px = mousex;
 //                py = mousey;
@@ -106,7 +123,7 @@ public class GameManager {
                 char rckey = (char) rkey;
                 // left right up down
                 if (rckey == '%' || rckey == '\'' || rckey == '&' || rckey == '(')
-                    cn.getTextWindow().output(px, py, 'P'); // VK kullanmadan test teknigi
+                    cn.getTextWindow().output(px, py, 'P', PLAYER_COLOR); // VK kullanmadan test teknigi
                 else
                     cn.getTextWindow().output(rckey);
 
@@ -128,7 +145,8 @@ public class GameManager {
                 px = rnd.nextInt(GAME_FIELD_X);
                 py = rnd.nextInt(GAME_FIELD_Y);
             }
-            cn.getTextWindow().output(px, py, 'P');
+            wholeGrid[px][py] = 'X';
+            cn.getTextWindow().output(px, py, 'X', ROBOT_COLOR);
         }
     }
 
@@ -137,7 +155,8 @@ public class GameManager {
             px = rnd.nextInt(GAME_FIELD_X);
             py = rnd.nextInt(GAME_FIELD_Y);
         }
-        cn.getTextWindow().output(px, py, 'P');
+        wholeGrid[px][py] = 'P';
+        cn.getTextWindow().output(px, py, 'P', PLAYER_COLOR);
     }
 
 
@@ -146,10 +165,10 @@ public class GameManager {
             for (int j = 0; j < GAME_FIELD_Y; j++) {
                 if (i == 0 || i == 54 || j == 0 || j == 24) {
                     wholeGrid[i][j] = '#'; // First row of the outer walls
-                    cn.getTextWindow().output(i, j, '#');
+                    cn.getTextWindow().output(i, j, '#', WALL_COLOR);
                 } else {
                     wholeGrid[i][j] = ':'; // First row of the outer walls
-                    cn.getTextWindow().output(i, j, ':');
+                    cn.getTextWindow().output(i, j, ':', EARTH_COLOR);
                 }
 
             }
@@ -165,7 +184,7 @@ public class GameManager {
                 wholeGrid[x][y] = ' ';
                 char boulds = 'O';
                 wholeGrid[x][y] = boulds;
-                cn.getTextWindow().output(x, y, boulds);
+                cn.getTextWindow().output(x, y, boulds, BOULDER_COLOR);
                 counterb++;
             }
         }
@@ -183,7 +202,7 @@ public class GameManager {
 //                //ASCII codes: 49=1_50=2_51=3
 //                char randomTreasure = (char) (rnd.nextInt(52 - 49) + 49);
 //                wholeGrid[randomi][randomj] = randomTreasure;
-//                cn.getTextWindow().output(randomi, randomj, randomTreasure);
+//                cn.getTextWindow().output(randomi, randomj, randomTreasure, TREASURE_COLOR);
 //                counterForTreasure++;
 //            } else if (wholeGrid[randomi][randomj] == ':' && counterForEmptySquares <= maxEmptySquares) {
 //                wholeGrid[randomi][randomj] = ' ';
